f737b4989803526f12352660e27e9a65d04560ab
pratik <lazy.monk.13@gmail.com>
2016-08-27 10:30:11 -0700

    first commit

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class HomePage extends Component {
+	render() {
+		return (
+			<div className="jumbotron">
+				<h1>Pratik's React-Redux Template</h1>
+				<p>Enjoy Responsibly</p>
+				<a href="about" className="btn btn-default btn-lg">Github Repo</a>
+			</div>
+		);
+	}
+}
+
+export default HomePage;
