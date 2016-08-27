e5a3782654eda2fbead7c896a75d259300bc19ff
pratik <lazy.monk.13@gmail.com>
2016-08-27 14:38:09 -0700

    google auth working

---
+++ src/FirebaseInit.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyCJW21pBv1X8MjVIyMjPtRcJDKPuMcCrg8",
+  authDomain: "ebayredux-247f8.firebaseapp.com",
+  databaseURL: "https://ebayredux-247f8.firebaseio.com",
+  storageBucket: "ebayredux-247f8.appspot.com"
+};
+
+firebase.initializeApp(config);
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,12 +1,44 @@
 import React, {Component} from 'react';
+import firebase from '../../FirebaseInit';
+
+const provider = new firebase.auth.GoogleAuthProvider();
 
 class HomePage extends Component {
+
+	constructor() {
+		super();
+
+		this.signIn = this.signIn.bind(this);
+	}
+
+	signIn() {
+		firebase.auth().signInWithPopup(provider)
+			.then(function(result) {
+			  // This gives you a Google Access Token. You can use it to access the Google API.
+			  var token = result.credential.accessToken;
+			  // The signed-in user info.
+			  var user = result.user;
+				console.log ('user:', user)
+			  // ...
+			}).catch(function(error) {
+			  // Handle Errors here.
+			  var errorCode = error.code;
+			  var errorMessage = error.message;
+			  // The email of the user's account used.
+			  var email = error.email;
+			  // The firebase.auth.AuthCredential type that was used.
+			  var credential = error.credential;
+			  // ...
+			});
+	}
+
 	render() {
 		return (
-			<div className="jumbotron">
-				<h1>Pratik's React-Redux Template</h1>
-				<p>Enjoy Responsibly</p>
-				<a href="about" className="btn btn-default btn-lg">Github Repo</a>
+			<div>
+				<h1>hello</h1>
+				<button onClick={this.signIn}>
+					Sign In
+				</button>
 			</div>
 		);
 	}
