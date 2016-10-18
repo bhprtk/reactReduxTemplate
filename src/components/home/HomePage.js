c37db05397d98415a69444d94e060706e6400730
pratik <lazy.monk.13@gmail.com>
2016-10-17 22:39:06 -0700

    first commit

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,19 +1,30 @@
 import React, {Component} from 'react';
-import firebase from '../../FirebaseInit';
-
-const provider = new firebase.auth.GoogleAuthProvider();
+import Geosuggest from 'react-geosuggest';
 
 class HomePage extends Component {
 
 	constructor() {
 		super();
+
+		this.inputChange = this.inputChange.bind(this);
 	}
 
+	inputChange(e) {
+		let input = e.target;
+		const autocomplete = new google.maps.places.Autocomplete(input);
+		console.log ('input:', input)
+	}
 
 	render() {
 		return (
 			<div>
 				<h1>hello</h1>
+				<input
+					id="pac-input"
+					className="controls"
+					type="text"
+        	placeholder="Enter a location"
+					onChange={this.inputChange} />
 			</div>
 		);
 	}
